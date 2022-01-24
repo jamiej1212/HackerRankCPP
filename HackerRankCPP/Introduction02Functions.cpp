f9995f7376af1f2d4b9a3b0974016454e6d5677e
jamiej1212 <jaydee12347@gmail.com>
2022-01-24 15:42:55 -0800

    Functions added

---
+++ HackerRankCPP/Introduction02Functions.cpp
@@ -0,0 +1,62 @@
+/*
+* Input Format
+
+Input will contain four integers -  , one per line.
+
+Output Format
+
+Return the greatest of the four integers.
+PS: I/O will be automatically handled.
+
+Sample Input
+
+3
+4
+6
+5
+Sample Output
+
+6
+*/
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d)
+{
+    int max;
+    if (a > b && a > c && a > d)
+    {
+        max = a;
+    }
+    else if (b > a && b > c && b > d)
+    {
+        max = b;
+    }
+    else if (c > a && c > b && c > d)
+    {
+        max = c;
+    }
+    else if (d > a && d > b && d > c)
+    {
+        max = d;
+    }
+
+    return max;
+
+
+    
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+
+    return 0;
+}
